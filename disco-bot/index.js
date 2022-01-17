ccbcf85a45098df28e606a7536036d143b8e0c9e
ChrisAshtear <chrisashtear@gmail.com>
2022-01-16 19:29:52 -0500

    working discobot in docker image.

--- disco-bot/index.js
+++ disco-bot/index.js
@@ -1,7 +1,10 @@
-require('dotenv').config(); //initialize dotenv
 const Discord = require('discord.js'); //import discord.js
 const axios = require('axios');
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] }); //create new client
+
+var prefix = "http://";
+if(process.env.BACKEND_HTTPS == "true"){ prefix = "https://";}
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -11,7 +14,7 @@ client.login(process.env.CLIENT_TOKEN); //login bot using token
 client.on('message', msg => {
   if (msg.content === '!date') {
 	
-  axios.get(process.env.API_SITE)
+  axios.get(prefix + process.env.API_HOST+":"+process.env.API_PORT+"/getserver")
   .then((response) => { msg.reply("Game Date is: "+(response.data[0].month)+"/"+(response.data[0].dayofmonth)+". The Apocalypse began "+response.data[0].daysSinceStart+" days ago.");})
 
   }
