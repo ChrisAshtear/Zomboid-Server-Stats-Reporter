a83ce627109db09ef5e1102a51ee2f5212dd4778
ChrisAshtear <chrisashtear@gmail.com>
2022-01-30 10:01:43 -0500

    imported coreui css added watch face style for time organized top of server info into cards.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import './cui.css';
 import axios from 'axios';
-import { Button, Container, Card, Row } from 'react-bootstrap'
+import {Card,CardDeck,CardBody,CardText,CardTitle,CardHeader,Row,Col,Container} from 'reactstrap';
 
 class App extends Component {
   constructor(props) {
 	  super(props);
       this.state = {
         fetchData: [],
-		servData: [],
+		servData: {dayofmonth:29,month:12,daysSinceStart:20,name:"ServerName",description:"description"},
 		servName: '',
       }
 	  window.app = this;
+	  window.host = process.env.BACKEND_HOST;
+	  window.port = process.env;
 	}
   
 	handleChange = (event) => {
@@ -43,28 +46,53 @@ class App extends Component {
 		  return (
 			  <React.Fragment>
 				  <Card style={{ width: '18rem' }} className='m-2'>
-					  <Card.Body>
-						  <Card.Title>{val.username}</Card.Title>
-						  <Card.Text>
+					  <CardBody>
+						  <CardTitle>{val.username}</CardTitle>
 							  {val.charname}
 							  <br/>
 							  Last Online - {x.toDateString()}
-						  </Card.Text>
-					  </Card.Body>
+					  </CardBody>
 				  </Card>
 			  </React.Fragment>
 		  )
 	  })
 
 	  return (
-		  <div className='App'>
-			  <h3>{this.state.servData.name}</h3>
+		  <div className='App animated-fadein'>
+			  <Card>
+			  <CardHeader ><h3>{this.state.servData.name}</h3></CardHeader>
+			  <CardBody>
 			  {this.state.servData.description}
-			  <br/>
-			  Its been {this.state.servData.daysSinceStart} days since the Apocalypse.
-			  <br/>
-			  The ingame date is {this.state.servData.month}/{this.state.servData.dayofmonth}
-				
+			  </CardBody>
+			  </Card>
+			  <Container>
+			  <CardDeck>
+			  <Card className='m-2'>
+					<CardHeader >
+                        In-Game Date
+                    </CardHeader>
+					  <CardBody className='clock-bg'>
+						  <CardText className='font-clock clock-bg h1'>{this.state.servData.month}/{this.state.servData.dayofmonth}</CardText>
+					  </CardBody>
+			  </Card>
+			  <Card className='m-2'>
+					<CardHeader >
+                        Current Players
+                    </CardHeader>
+					  <CardBody>
+						  <CardText className='h1'>15/32</CardText>
+					  </CardBody>
+			  </Card>
+			  <Card className='m-2'>
+					<CardHeader >
+                        Days Since Apocalypse
+                    </CardHeader>
+					  <CardBody>
+						  <CardText className='h1'>{this.state.servData.daysSinceStart}</CardText>
+					  </CardBody>
+			  </Card>
+			  </CardDeck>
+			  </Container>
 			  <Container>
 				  <Row>
 					  {card}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+const axios = require('axios');
 
 ReactDOM.render(
   <App />,
