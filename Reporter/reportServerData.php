87e7d4bb60c56201105dad66ee0d3e284f87c7ab
ChrisAshtear <chrisashtear@gmail.com>
2022-01-30 20:59:39 -0500

    updated reporter to grab start date,max players, and calculate the current year.
    lua parser from https://github.com/KeldorDE/Lightweight-PHP-LUA-Parser

--- Reporter/reportServerData.php
+++ Reporter/reportServerData.php
@@ -1,13 +1,14 @@
 <?php
 
 include "parseData.php";
-//have to push system environment values to a file on boot to allow them to be read by php
+require('luaparser.php');
+
 $env = parse_ini_file("/etc/environment",false,INI_SCANNER_RAW);
 //get name of server
 $SERVER_NAME = $env['ZOMBOID_SERVER_NAME'];
 chdir('/var/www/html');
 
-class PZplayer_DB extends SQLite3
+class MyDB extends SQLite3
 {
     function __construct()
     {
@@ -15,9 +16,7 @@ class PZplayer_DB extends SQLite3
     }
 }
 
-
-
-class PZserver_DB extends SQLite3
+class MyPZ_DB extends SQLite3
 {
     function __construct()
     {
@@ -26,27 +25,24 @@ class PZserver_DB extends SQLite3
 }
 
 
-$player_db = new PZplayer_DB();
-$server_db = new PZserver_DB();
+$db = new MyDB();
+$pz_db = new MyPZ_DB();
 
-//Read network players and update player entries in SQL db.
-
-$result = $player_db->query('SELECT * FROM networkPlayers');
+$result = $db->query('SELECT * FROM networkPlayers');
 
 while ($row = $result->fetchArray()) 
 {
-	//The last connection values are stored in the serverdb table and stored by username. 
-    $pzLookup = $server_db->query('SELECT username,lastConnection FROM whitelist WHERE username = "'.$row['username'].'"');
+    $pzLookup = $pz_db->query('SELECT username,lastConnection FROM whitelist WHERE username = "'.$row['username'].'"');
     $r = $pzLookup->fetchArray();
     $row['lastOnline'] = $r['lastConnection'];
-	if($r['lastConnection'] == ""){$row['lastOnline']= date(DATE_ATOM, mktime(0, 0, 0, 7, 1, 2000));}//default time in case there is no value
+	if($r['lastConnection'] == ""){$row['lastOnline']= date(DATE_ATOM, mktime(0, 0, 0, 7, 1, 2000));}
     $d = new parseData($row);
     $d->parse($row);
     $d->runQuery("Players",$d->sqlGetID("id","Players","id",$row['id'])); 
 }
 $d->closeSQL();
-
-//read binary time file
+echo("trying file read");
+//read time file
 $filename = "sv/Saves/Multiplayer/".$GLOBALS['SERVER_NAME']."/map_t.bin";
 $handle = fopen($filename,"rb");
 $contents = fread($handle, filesize($filename));
@@ -60,14 +56,42 @@ $block_size = 3;
 // figure out the number of blocks in the file
 $block_count = $file_size/$block_size;
 
-$day = intval($unpacked[31]) + 1;//32nd byte is the CurrentDay value in the zomboid save file. Add+1 to it because day 1 has a value of 0.
-$month = intval($unpacked[35]) + 1;//35th byte is the CurrentMonth value in the zomboid save file. Add+1 to it because january has a value of 0.
+$day = intval($unpacked[31]) + 1;//day and month are 0-values. $month=0 is january
+$month = intval($unpacked[35]) + 1;
 echo("DayOfMonth:".$day);
 echo("Month:".$month);
-echo("DaysSurvived:".$unpacked[15]);//15th byte is DaysSurvived.
+echo("DaysSurvived:".$unpacked[15]);
 
-//Read server settings from INI file. (Name and Description)
+//INI File
 $server = parse_ini_file("sv/Server/".$GLOBALS['SERVER_NAME'].".ini",false,INI_SCANNER_RAW);
+//Sandbox LUA file
+$sandbox = new LUAParser();
+
+try {
+
+	// Add LUA keys that have to be present in the file
+	// If one of the defined keys are missing an syntax error exception will be thrown
+	$sandbox->addSyntaxKey('StartDay');
+	$sandbox->addSyntaxKey('StartYear');
+	$sandbox->addSyntaxKey('StartMonth');
+
+	$sandbox->parseFile("sv/Server/".$GLOBALS['SERVER_NAME']."_SandboxVars.lua");
+}
+catch(Exception $e) {
+    echo 'Exception: ',  $e->getMessage(), PHP_EOL;
+}
+
+//set current year.
+
+$startYear = $sandbox->data['SandboxVars']['StartYear']-1+1993;//1993 is the year Zomboid takes place.
+$startMonth = $sandbox->data['SandboxVars']['StartMonth'];
+$startDay = $sandbox->data['SandboxVars']['StartDay'];
+$daysIntoYear = date('z',strtotime($startMonth.'/'.$startDay)) + 1;
+echo("days into year:".$daysIntoYear);
+$daysSinceStart = $unpacked[15];
+$curYear = floor(($daysSinceStart + $daysIntoYear) / 365) + $startYear;
+
+
 echo($server["PublicName"]);
 
     $sfield['sid'] = 1;
@@ -76,6 +100,12 @@ echo($server["PublicName"]);
     $sfield['daysSinceStart'] = $unpacked[15];
     $sfield['sname'] = $server["PublicName"];
     $sfield['desc'] = $server["PublicDescription"];
+	$sfield['maxPlayers'] = $server["MaxPlayers"];
+	$sfield['startDay'] = $startDay;
+	$sfield['startMonth'] = $startMonth;
+	$sfield['startYear'] = $startYear;
+	$sfield['year'] = $curYear;
+	
     $d = new parseData($sfield);
     $d->parse($sfield);
     $d->runQuery("Game",$d->sqlGetID("id","Game","id",$sfield['sid']));
